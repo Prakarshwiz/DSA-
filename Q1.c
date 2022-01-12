f61797ee82e2568565bd05842e13b3b4f3039e6f
Prakarsh Pathak <73680865+Prakarshwiz@users.noreply.github.com>
2022-01-12 21:27:09 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+{
+    int n,i,j;
+    printf("enter no. of lines:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        for(j=0;j<=i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
